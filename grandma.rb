ec8eb4f2b54bb0f1f574ab174094884828bf52bb
H B <phajib@gmail.com>
2019-01-09 10:24:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     phrase = "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     phrase = "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == phrase.upcase
     phrase = "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
     phrase = "NO, NOT SINCE 1938!"
